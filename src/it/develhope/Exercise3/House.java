ba169c1eadcaf30746ac04bdf1d8e3cecbe65b36
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-17 15:13:11 +0100

    Exercise: constructor-1

---
+++ src/it/develhope/Exercise3/House.java
@@ -0,0 +1,12 @@
+package it.develhope.Exercise3;
+
+public class House {
+    public String address;
+    public int numberOfFloors;
+
+    public void house(String address, int numberOfFloors){
+        this.address = address;
+        this.numberOfFloors = numberOfFloors;
+        System.out.println("The object house has been created");
+    }
+}
---
+++ src/it/develhope/Exercise3/Tester.java
@@ -0,0 +1,11 @@
+package it.develhope.Exercise3;
+
+public class Tester {
+    public static void main(String[] args) {
+        House house1 = new House();
+        House house2 = new House();
+
+        house1.house("Street Roma,3",7);
+        house2.house("Street Garibaldi,73",5);
+    }
+}
