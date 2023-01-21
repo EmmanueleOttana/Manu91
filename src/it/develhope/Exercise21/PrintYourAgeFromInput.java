6f3fb06f01f5f3ddb63fbc2257c50e4ad11c2fe6
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-22 00:23:08 +0100

    Exercise: input-and-output-5

---
+++ src/it/develhope/Exercise21/PrintYourAgeFromInput.java
@@ -0,0 +1,14 @@
+package it.develhope.Exercise21;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintYourAgeFromInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = input.nextInt();
+        System.out.printf("Your age is: %d",age);
+        input.close();
+    }
+}
