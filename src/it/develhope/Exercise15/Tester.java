aa5eb325ef7f75e8d4bce9e1a519d07cd74832dd
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-21 20:34:29 +0100

    Exercise: If Statement 3

---
+++ src/it/develhope/Exercise15/Tester.java
@@ -0,0 +1,13 @@
+package it.develhope.Exercise15;
+
+import java.util.Arrays;
+
+public class Tester {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.constructor("Emmanuele",31);
+
+        student.guessingAge();
+    }
+}
