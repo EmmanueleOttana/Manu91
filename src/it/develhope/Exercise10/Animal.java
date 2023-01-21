70fb029777459da36015dae619562aba85154714
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-21 01:52:57 +0100

    Exercise: Polymorphism 2

---
+++ src/it/develhope/Exercise10/Animal.java
@@ -0,0 +1,12 @@
+package it.develhope.Exercise10;
+
+public class Animal {
+    String animalName;
+    public void constructor(String animalName){
+        this.animalName = animalName;
+    }
+    public void animalSound(){
+        System.out.println("Default animal sound");
+    }
+
+}
---
+++ src/it/develhope/Exercise10/Cow.java
@@ -0,0 +1,8 @@
+package it.develhope.Exercise10;
+
+public class Cow extends Animal{
+    @Override
+    public void animalSound() {
+        System.out.println("The sound of the cow is Graaaa");
+    }
+}
