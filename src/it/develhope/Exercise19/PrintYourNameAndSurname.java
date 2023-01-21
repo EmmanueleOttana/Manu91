67d96ee785f4c2784e908b9f328cb114efa713ab
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-21 23:51:44 +0100

    Exercise: input-and-output-3

---
+++ src/it/develhope/Exercise19/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package it.develhope.Exercise19;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.println("Emmanuele ");
+        System.out.println("Ottanà");
+    }
+}
