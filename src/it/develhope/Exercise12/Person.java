ccdd3a95b6c7643293f106f6cda91ade46879221
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-21 15:53:34 +0100

    Exercise: Encapsulation 2

---
+++ src/it/develhope/Exercise12/Person.java
@@ -0,0 +1,34 @@
+package it.develhope.Exercise12;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Person {
+    static String name;
+    static String surname;
+    static double height;
+    static int age;
+
+    static void detailsPerson(){
+        System.out.println("Person{"+
+        " name = "+ name+
+        " surname = "+surname+
+        " height = "+height+
+        " age = "+ age);
+    }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter name");
+        name = scanner.nextLine();
+        System.out.println("Please enter last name");
+        surname = scanner.nextLine();
+        System.out.println("Please enter height");
+        height = scanner.nextDouble();
+        System.out.println("Please enter age");
+        age = scanner.nextInt();
+
+        detailsPerson();
+    }
+}
