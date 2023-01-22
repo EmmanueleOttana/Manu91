456338c4f05a2ba254370f6207b654b5fbb04661
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-22 23:05:53 +0100

    Exercise: Boolean Operators 4

---
+++ src/it/develhope/Exercise27/Boolean4.java
@@ -0,0 +1,16 @@
+package it.develhope.Exercise27;
+
+public class Boolean4 {
+    public static void main(String[] args) {
+
+        int a = 1, b = 3, c = a * b, x = 5, y = 6, d = c;
+        boolean t = false, f = true;
+
+        boolean A = (d / c + 2) >= b || !(c + b - c / a == 3);       //True
+        boolean B = (x * x - y * y / 2 != 12) || !t && f;            //True
+
+        System.out.println(A);
+        System.out.println(B);
+
+    }
+}
