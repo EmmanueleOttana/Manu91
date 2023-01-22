f49f9cf517b91f5d8b1792dd5b3954f96b1fe71b
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-22 23:40:22 +0100

    Exercise: Other operators

---
+++ src/it/develhope/Exercise28/OtherOperators.java
@@ -0,0 +1,18 @@
+package it.develhope.Exercise28;
+
+public class OtherOperators {
+    public static void main(String[] args) {
+        int a = 15;
+        a += 5;
+        a -= 4;
+
+        int b = a;
+        b++;
+
+        if (b % 2 != 0) System.out.println("'B' is an odd number");
+        else System.out.println("'B' is an even number");
+
+        if (b * b +1 % 3 == 0) System.out.println("is a multiple of 3");
+        else System.out.println("(B * B +1) is not a multiple of 3");
+    }
+}
