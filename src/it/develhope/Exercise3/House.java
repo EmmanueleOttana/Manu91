f849edea46c4be851a6a6d5bc7cc9e09fa462d92
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-23 15:09:17 +0100

    Exercise: constructor-1

--- src/it/develhope/Exercise3/House.java
+++ src/it/develhope/Exercise3/House.java
@@ -4,7 +4,7 @@ public class House {
     public String address;
     public int numberOfFloors;
 
-    public void house(String address, int numberOfFloors){
+    public House(String address, int numberOfFloors){
         this.address = address;
         this.numberOfFloors = numberOfFloors;
         System.out.println("The object house has been created");
--- src/it/develhope/Exercise3/Tester.java
+++ src/it/develhope/Exercise3/Tester.java
@@ -2,10 +2,7 @@ package it.develhope.Exercise3;
 
 public class Tester {
     public static void main(String[] args) {
-        House house1 = new House();
-        House house2 = new House();
-
-        house1.house("Street Roma,3",7);
-        house2.house("Street Garibaldi,73",5);
+        House house1 = new House("Street Roma,3",7);
+        House house2 = new House("Street Garibaldi,73",5);
     }
 }
