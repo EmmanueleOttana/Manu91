385a4a63f8101776f5610f404924dee542e20d3e
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-26 16:53:28 +0100

    Exercise: String handling 3

---
+++ src/it/develhope/Exercise31/StringHandling3.java
@@ -0,0 +1,23 @@
+package it.develhope.Exercise31;
+
+import java.util.Arrays;
+
+public class StringHandling3 {
+    public static void main(String[] args) {
+        String[] array1 = {" I want ",
+                " To learn ",
+                " how to code! "};
+        String[] array2 = new String[array1.length];
+
+        for(int i = 0; i < array1.length; i++){
+            array2[i] = array1[i].trim();
+        }
+
+        int statement1 = array2[0].compareTo(array1[0]);
+        int statement2 = array2[1].compareToIgnoreCase(array1[1]);
+
+        System.out.println("Is statement1 greater than statement2? " + (statement1>statement2));
+
+
+    }
+}
