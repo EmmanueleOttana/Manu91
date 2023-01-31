6972220234a68f4c716bdd3ff9cbf92b326fe12b
Emmanuele Ottanà <manu91eo@gmail.com>
2023-01-31 13:02:01 +0100

    Exercise: Encapsulation 1

--- src/it/develhope/Exercise11/House.java
+++ src/it/develhope/Exercise11/House.java
@@ -1,27 +1,34 @@
 package it.develhope.Exercise11;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class House {
     private int floorsNumber;
     private String address;
-    private String residentsNames;
-    Scanner scanner = new Scanner(System.in);
+    private String[] residentsNames;
+
+    public void setFloorsNumber(int floorsNumber) {
+        this.floorsNumber = floorsNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setResidentsNames(String[] residentsNames) {
+        this.residentsNames = residentsNames;
+    }
 
     public void getAddress() {
         System.out.println("Please enter the street of the residents");
-        this.address = scanner.nextLine();
     }
     public void getFloorsNumber() {
         System.out.println("How many floors does the resident have?");
-        this.floorsNumber = scanner.nextInt();
-        scanner.nextLine();
     }
     public void getResidentsNames(){
         System.out.println("Please enter names of residents?");
-        this.residentsNames = scanner.nextLine();
     }
     public void getPrint(){
-        System.out.println(String.format("The residence is located in %s, has %d floors and it's residents are: %s",address,floorsNumber,residentsNames));
+        System.out.println(String.format("The residence is located in %s, has %d floors and it's residents are: %s",address,floorsNumber, Arrays.toString(residentsNames)));
     }
 }
--- src/it/develhope/Exercise11/Tester.java
+++ src/it/develhope/Exercise11/Tester.java
@@ -1,12 +1,26 @@
 package it.develhope.Exercise11;
 
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         House house = new House();
 
         house.getAddress();
+        String address = scanner.nextLine();
+
         house.getFloorsNumber();
+        int floorsNumber = scanner.nextInt();
+        scanner.nextLine();
+
         house.getResidentsNames();
+        String[] residentsNames = scanner.nextLine().split(" ");
+
+        house.setAddress(address);
+        house.setFloorsNumber(floorsNumber);
+        house.setResidentsNames(residentsNames);
+
         house.getPrint();
     }
 }
