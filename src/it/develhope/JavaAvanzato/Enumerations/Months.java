365f025a7f0f39d46ac1e4d976c3119a3759ec84
Emmanuele Ottanà <manu91eo@gmail.com>
2023-02-05 20:58:32 +0100

    Exercise: Enumerations

---
+++ src/it/develhope/JavaAvanzato/Enumerations/Months.java
@@ -0,0 +1,17 @@
+package it.develhope.JavaAvanzato.Enumerations;
+
+public class Months {
+    public static void main(String[] args) {
+
+        enum month {
+            January, February, March, April, May, June,
+            July, August, September, October, November, December
+        }
+
+        for (month value : month.values()) {
+            if (String.valueOf(value).endsWith("y")){
+                System.out.println(value + " ends with y");
+            }else System.out.println(value + " doesn't end with y");
+        }
+    }
+}
