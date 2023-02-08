dcbaeda04c8041e79ef58d6244ee086a669f1087
Emmanuele Ottanà <manu91eo@gmail.com>
2023-02-08 16:25:34 +0100

    Exercise develhope

---
+++ src/it/develhope/javaAvanzato/forLoop2/forLoop2.java
@@ -0,0 +1,18 @@
+package it.develhope.javaAvanzato.forLoop2;
+
+public class forLoop2 {
+    public static void main(String[] args) {
+
+        {
+         int seqFib = 1;
+         int supp1 = 0;
+         int supp2 = 0;
+            for (int i = 0; i < 10; i++) {
+                System.out.println(seqFib);
+                supp2 = supp1;
+                supp1 = seqFib;
+                seqFib = supp1 + supp2;
+            }
+        }
+    }
+}
