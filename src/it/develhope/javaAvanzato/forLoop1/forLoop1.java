1f0729320ac98a9948fde87a848c76b2c5b96367
Emmanuele Ottanà <manu91eo@gmail.com>
2023-02-08 14:45:45 +0100

    Exercise develhope

--- src/it/develhope/javaAvanzato/forLoop1/forLoop1.java
+++ src/it/develhope/javaAvanzato/forLoop1/forLoop1.java
@@ -2,6 +2,7 @@ package it.develhope.javaAvanzato.forLoop1;
 
 public class forLoop1 {
     public static void main(String[] args) {
+
         for (int i = 5; i <= 25; i++) {
             System.out.println(i);
         }
