18f750b17d9bd23ad94c1cf0394f3dd5eca82d32
Emmanuele Ottanà <manu91eo@gmail.com>
2023-02-11 15:04:32 +0100

    ExerciseDevelhope

--- src/it/develhope/javaAvanzato/switch2/Switch2.java
+++ src/it/develhope/javaAvanzato/switch2/Switch2.java
@@ -1,2 +1,34 @@
-package it.develhope.javaAvanzato.switch2;public class Switch2 {
+package it.develhope.javaAvanzato.switch2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Switch2 {
+    public static void main(String[] args) {
+
+        List list = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please enter a command:");
+            String string = scanner.nextLine();
+
+            if (!string.equalsIgnoreCase("stop")) {
+                switch (string.toLowerCase()) {
+                    case "size":
+                        System.out.println(list.size());
+                        break;
+                    case "clear":
+                        list.clear();
+                        break;
+                    case "print":
+                        System.out.println(list.toString());
+                        break;
+                    default:
+                        list.add(string);
+                }
+            } else break;
+        }
+    }
 }
