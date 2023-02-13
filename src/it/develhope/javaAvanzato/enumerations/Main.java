b391390c9f4e4696e4d1d9886006232ddfd7ea02
Emmanuele Ottanà <manu91eo@gmail.com>
2023-02-13 14:13:17 +0100

    Exercise develhope

--- src/it/develhope/javaAvanzato/enumerations/Main.java
+++ src/it/develhope/javaAvanzato/enumerations/Main.java
@@ -1,14 +1,9 @@
 package it.develhope.javaAvanzato.enumerations;
 
-public class Months {
+public class Main {
     public static void main(String[] args) {
 
-        enum month {
-            January, February, March, April, May, June,
-            July, August, September, October, November, December
-        }
-
-        for (month value : month.values()) {
+        for (Month value : Month.values()) {
             if (String.valueOf(value).endsWith("y")){
                 System.out.println(value + " ends with y");
             }else System.out.println(value + " doesn't end with y");
---
+++ src/it/develhope/simEsame/marco/Wizard.java
@@ -0,0 +1,2 @@
+package it.develhope.simEsame.marco;public class Wizard {
+}
