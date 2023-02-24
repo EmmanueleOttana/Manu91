8cde67f20728a0b37fd190c3d9781d9e86d44d09
Emmanuele Ottanà <manu91eo@gmail.com>
2023-02-24 20:27:55 +0100

    Exercise House

---
+++ src/it/develhope/eserciziCasa/PositionInArray.java
@@ -0,0 +1,40 @@
+package it.develhope.eserciziCasa;
+
+public class PositionInArray {
+
+    static void x (int target, int[] arr){
+        int i = 0;
+        for ( int value : arr) {
+            if ( value == target) {
+                System.out.println(i);
+            }
+            i ++;
+        }
+        i = 0;
+        int print = 0;
+        for ( int value : arr) {
+            if ( value > target) {
+                print ++;
+                System.out.println(i);
+                break;
+            }
+            i ++;
+        }
+        if (print == 0) { System.out.println(i); }
+    }
+
+    public static void main(String[] args) {
+
+        int [] nums = {1,3,5,6};
+
+        x(10,nums);
+
+    }
+}
+/*
+Dato un array ordinato di numeri interi distinti
+ e un valore target, restituisce l'indice se
+ viene trovato il target. In caso contrario,
+  restituire l'indice dove sarebbe se
+  fosse inserito in ordine.
+*/
